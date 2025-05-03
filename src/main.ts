50d93346e804121b7382b423a2a6ea270868017e
Mert Celik <mertcelik2399@gmail.com>
2025-05-03 21:12:42 +0300

    Revert "renaming"
    This reverts commit 3ac8a9a3ca3d68ea0580f974240bbc60b2a2fe6e.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { config } from 'dotenv';
 import { createApiRouter } from './api/routes';
-import {errorHandler, notFoundHandler} from "@core/middleware/ErrorHandler";
 
 config();
 
@@ -20,23 +19,7 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'okay' });
 });
 
-app.use(notFoundHandler);
-app.use(errorHandler);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-
-process.on('uncaughtException', (error: Error) => {
-  console.error('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.error(error.name, error.message, error.stack);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (error: Error) => {
-  console.error('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.error(error.name, error.message, error.stack);
-  process.exit(1);
 });
